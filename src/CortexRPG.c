e09c91687f9f5bc87614ac09d0a1ba4fe873f679
KyleRS2004 <122418673+KyleRS2004@users.noreply.github.com>
2023-01-15 09:40:03 -0500

    Update CortexRPG.c
    The Dice system isn't fully working.  I don't understand how to go about setting a max value to the random value generated with rand().

--- src/CortexRPG.c
+++ src/CortexRPG.c
@@ -1,15 +1,19 @@
 //Include Libraries
 #include <stdio.h>
+#include <stdlib.h>
 
 //Sets up intial Player Stats
-char* Player="playername";
+char* Player="playername\0";
 int health=100;
 int strength=0;
 int wisdom=0;
 int stamina=0;
 
+//Dice
+float diceFloat;
+
 //GoingDirection
-char* playerDirect="default";
+;char* playerDirect="default\0";
 
 //Loaded Map Environment
 int mapWidth=400;
@@ -18,26 +22,31 @@ int mapHeight=400;
 int spawnSpotX=200;
 int spawnSpotY=200;
 
+                              // mapGen() is the the function you call once you set the play's playerDirect variable.
 void mapGen(){
-    if (playerDirect="north") // Player Goes North
+    if (playerDirect="north\0") // Player Goes North
     {
         int spawnSpotX=200;
         int spawnSpotY=0;
+        char* playerDirect="default\0";
     }
-    if (playerDirect="south") // Player Goes South
+    if (playerDirect="south\0") // Player Goes South
     {
         int spawnSpotX=200;
         int spawnSpotY=400;
+        char* playerDirect="default\0";
     }
-    if (playerDirect="east")  // Player Goes East
+    if (playerDirect="east\0")  // Player Goes East
     {
         int spawnSpotX=400;
         int spawnSpotY=200;
+        char* playerDirect="default\0";
     }
-    if (playerDirect="west")  // Player Goes West
+    if (playerDirect="west\0")  // Player Goes West
     {
         int spawnSpotX=0;
         int spawnSpotY=200;
+        char* playerDirect="default\0";
     }
 
 }
@@ -45,10 +54,26 @@ void mapGen(){
 //Save System
 
 
-//Custom Functions
+//Dice System
+void DiceGen(){
+    float diceFloat = rand() % 1;   //Dice need worked on, they don't seem to have a max no matter what I do.
+}
+
+
+
+//Player Stat Checks
 // Checks Stength stat for action
 void StrengthCheck() {
     printf("%d", strength);
+    DiceGen();
+    if (diceFloat>strength)
+    {
+        printf("Skill Check Success.");
+    }
+    else
+    {
+        printf("Skill Check Failure");
+    }
 }
 
 // Checks Wisdom stat for action
